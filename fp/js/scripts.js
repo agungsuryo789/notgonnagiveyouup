be506e2f026c1c15b25c723423c75388bf66c9d7
MariaYonessaPurwalasari <maria.purwalasari@students.amikom.ac.id>
2018-06-05 01:23:04 +0700

    contact baru header

---
+++ fp/js/scripts.js
@@ -0,0 +1,33 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */    
+    $.backstretch([
+                           "img/grey.png"
+                         ], { fade: 750});
+    
+    /*
+        Form validation
+    */
+    $('.registration-form input[type="text"], .registration-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.registration-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+});
